2ee095bf4ddbbc8d905723fe987c6df6fab71c4c
Martin Mendez <martinpercu@gmail.com>
2023-07-19 19:16:27 +0100

    starting repository

---
+++ environment.js
@@ -0,0 +1,8 @@
+let hello = 'bonjour ';
+
+let names = process.env.NAMES || 'Sans Prenom';
+
+
+console.log(hello + names);
+
+console.log('Hello' + names);
---
+++ monofil.js
@@ -0,0 +1,16 @@
+console.log('Bonjour oplanete');
+
+let i = 0;
+setInterval(function() {
+    console.log(i);
+    i++;
+
+}, 600);
+
+console.log(('otra intruction'));
+
+setInterval(function() {
+    console.log(i + 'sdfdsf');
+    i++;
+
+}, 900);
