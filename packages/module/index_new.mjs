ab61f82573e25e8d4d4575755d5c5224ac21430a
Martin Mendez <martinpercu@gmail.com>
2023-08-09 21:24:17 -0400

    the new and the old way to import modules

---
+++ packages/module/index_new.mjs
@@ -0,0 +1,6 @@
+import moduleNew from './module_new.mjs';
+
+console.log(moduleNew);
+
+moduleNew();
+
---
+++ packages/module/module_old.js
@@ -0,0 +1,8 @@
+function sayHello() {
+    console.log('Hello friends!!!');
+}
+
+module.exports = {
+    sayHello,
+    sayByeByre: 'Bye bye BYE BYE friends'
+}
